7e40125a723dfa17e02ddf9cf48f74c4767848fa
古屋 郷 <gou@koya-no-MacBook-Air.local>
2017-01-09 01:18:26 +0900

    Multiple components displays by v-for

---
+++ vue/components/hello.vue
@@ -0,0 +1,13 @@
+Vue.component('hello', {
+  props: ['message'],
+  template: '\
+    <div>\
+      <h1>Hello, {{ showMessage }}</h1>\
+    </div>\
+  ',
+  computed: {
+    showMessage: function() {
+      return this.message === undefined ? '___' : this.message
+    }
+  }
+})
